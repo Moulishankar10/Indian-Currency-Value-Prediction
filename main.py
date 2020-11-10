420b300a1de7b26d3eb45a1336fa361aa15805c0
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-10 20:00:42 +0530

    Updated code

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures

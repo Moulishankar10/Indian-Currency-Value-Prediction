5c5cc88500917a4503cadcebab88eecf7f75b916
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-10 20:09:15 +0530

    Updated code

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ import datetime as dt
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+
+data = pd.read_csv(data/data.csv)
+
+x = []
+initial_str = data["Month"][0]
+initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
+
+for i in range(len(data["Month"])):
+    final_str = data["Month"][i]
+    final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
+    diff = (final.year - initial.year) * 12 + (final.month - initial.month)
+    x.append(diff)

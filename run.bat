538d7d026ed00fe59d613ac1715b6d2c7cea858d
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-18 08:44:00 +0530

    Created run.bat

---
+++ run.bat
@@ -0,0 +1,13 @@
+"  HAPPY TO HAVE YOU ON INDIAN CURRENCY VALUE PREDICTION ! "
+
+" Checking the Required Dependencies ! "
+
+pip install numpy
+pip install pandas
+pip install sklearn
+pip install datetime
+pip install matplotlib
+
+"Now, you are all set to execute this program!"
+
+python main.py

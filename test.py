50203b45a0133c3b6d893205a4cef300df8394ba
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-19 17:06:06 +0530

    Imporetd data in test.py

--- test.py
+++ test.py
@@ -14,3 +14,20 @@ from sklearn.model_selection import train_test_split
 
 # IMPORTING DATA
 data = pd.read_csv("data/data.csv")
+
+# PREPROCESSING DATA
+x = []
+initial_str = data["Month"][0]
+initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
+ 
+for i in range(len(data["Month"])):
+    final_str = data["Month"][i]
+    final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
+    diff = (final.year - initial.year) * 12 + (final.month - initial.month)
+    x.append(diff)
+
+y = data["Price"].values
+
+# RESHAPING THE DATA
+x = np.reshape(x, (-1,1))
+y = np.reshape(y, (-1,1))

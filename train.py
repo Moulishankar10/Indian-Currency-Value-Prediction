e4b545ad4f0c43596cff6ab6c6c77141fb34eb46
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-19 16:08:58 +0530

    Prepared data for the model

--- train.py
+++ train.py
@@ -20,3 +20,29 @@ import matplotlib.pyplot as plt
 
 # IMPORTING DATA
 data = pd.read_csv("data/data.csv")
+
+# PREPROCESSING DATA
+x = []
+initial_str = data["Month"][0]
+initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
+ 
+for i in range(len(data["Month"])):
+    final_str = data["Month"][i]
+    final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
+    diff = (final.year - initial.year) * 12 + (final.month - initial.month)
+    x.append(diff)
+
+y = data["Price"].values
+
+# SPLITTING THE TRAINING AND VALIDATION DATA
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
+
+# RESHAPING THE DATA
+x_train = np.reshape(x_train, (-1,1))
+x_val = np.reshape(x_val, (-1,1))
+y_train = np.reshape(y_train, (-1,1))
+y_val = np.reshape(y_val, (-1,1))
+
+# SCALING THE DATA
+scaler_x = MinMaxScaler()
+scaler_y = MinMaxScaler()

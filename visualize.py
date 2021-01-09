4cd65b641b9e03ed60774bbfdb7c21569b19a910
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-09 18:07:53 +0530

    Modified the code

--- visualize.py
+++ visualize.py
@@ -13,20 +13,20 @@ from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
 # IMPORTING DATA
-data = pd.read_csv("data/sea-data.csv")
+data = pd.read_csv("data/data.csv")
 
 # PREPROCESSING DATA
 x = []
-initial_str = data["Time"][0]
+initial_str = data["Month"][0]
 initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
  
-for i in range(len(data["Time"])):
-    final_str = data["Time"][i]
+for i in range(len(data["Month"])):
+    final_str = data["Month"][i]
     final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
     diff = (final.year - initial.year) * 12 + (final.month - initial.month)
     x.append(diff)
 
-y = data["GMSL"].values
+y = data["Price"].values
 
 # RESHAPING THE DATA
 x = np.reshape(x, (-1,1))

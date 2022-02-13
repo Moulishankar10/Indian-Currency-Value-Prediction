5a6203023ec2243f08d25e6353b5e31676174d8f
Moulishankar10 <moulishankarmr@gmail.com>
2022-02-13 19:59:49 +0530

    Updated the training code

--- train.py
+++ train.py
@@ -13,16 +13,15 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-from tensorflow.keras.models import save_model
 
 # IMPORTING DATA
-data = pd.read_csv("data/data.csv")
+data = pd.read_csv("data.csv")
 
 # PREPROCESSING DATA
 x = []
 initial_str = data["Month"][0]
 initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
- 
+
 for i in range(len(data["Month"])):
     final_str = data["Month"][i]
     final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
@@ -48,7 +47,7 @@ xtrain_scaled = scaler_x.fit_transform(x_train)
 ytrain_scaled = scaler_y.fit_transform(y_train)
 xval_scaled = scaler_x.fit_transform(x_val)
 
-# BUILDING THE NEURAL NETWORK
+# DESIGNING THE NEURAL NETWORK
 model = Sequential()
 model.add(Dense(2, input_dim = 1, activation = 'relu', kernel_initializer='normal'))
 model.add(Dense(70, activation = 'relu'))
@@ -58,7 +57,7 @@ model.add(Dense(1, activation = 'linear'))
 
 # TRAINING THE DATA
 model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse', 'mae', 'accuracy'])
-history = model.fit(xtrain_scaled, ytrain_scaled, epochs = 100, batch_size = 50, validation_split = 0.1, verbose = 1)
+history = model.fit(xtrain_scaled, ytrain_scaled, epochs = 100, batch_size = 70, validation_split = 0.1, verbose = 1)
 print("\n\n ----- Model is trained successfully ! ----- \n\n")
 
 # VISUALISING THE MODEL LOSS
@@ -73,9 +72,9 @@ plt.show()
 # ACCURACY OF THE MODEL
 ypred_scaled = model.predict(xval_scaled)
 y_pred = scaler_y.inverse_transform(ypred_scaled)
-print(f"\nAccuracy of the model : {round(r2_score(y_val, y_pred)*100,2)}%")
+acc_score = r2_score(y_val, y_pred)
+print(f"\nAccuracy of the model : {round(acc_score*100,2)}%")
 
 # SAVING THE TRAINED MODEL
-PATH = './model/model'
-save_model(model,PATH)
-print(f"\n\n ---- Successfully stored the trained model at {PATH} ---- \n\n")
+model.save("model.h5")
+print(f"\n\n ---- Successfully stored the trained model ---- \n\n")
